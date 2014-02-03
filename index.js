f7192200d8624334627209132e4b43c21f9bec43
Patrick F. Marques <patrick.marques@k2c.eu>
2014-02-03 18:21:06 +0000

    Implement password generation for TURN REST API

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ function genLongTermKey( username, realm, password ) {
 	var hash = md5.digest( 'hex' );
 
 	return hash;
+}
 
-	return;
+function genSharedKey( username, shared_key ) {
+	var hmac = crypto.createHmac( 'sha1', shared_key );
+
+	hmac.update( username );
+
+	var hash = hmac.digest( 'base64' );
+
+	return hash;
 }
 
 module.exports = {
 	genLongTermKey : genLongTermKey,
+	genSharedKey : genSharedKey,
 }
