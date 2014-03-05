b211f5bf2809d3aa9e6abd76190564ef4029a0ab
Patrick F. Marques <patrick.marques@k2c.eu>
2014-03-05 16:36:15 +0000

    Add ttl from confs

--- index.js
+++ index.js
@@ -6,22 +6,29 @@ var keys = require( './lib/keys' );
 
 var SEP = keys.SEP;
 
+var DEFAULT_TTL = 86400;
+
 function TURN_REST_API( conf ) {
 
-	var store = new Store( { redis : conf.db } );
+	var ttl = conf.ttl || DEFAULT_TTL;
+
+	var store = new Store( {
+		redis : conf.db,
+		ttl   : ttl
+	});
 
 	function getTurn( username ) {
-		var csk = store.getCurrentSharedKey();
-		var hash = keys.genSharedKey( username, csk );
-
 		/* Define expiration timestamp */
 		var timestamp = Math.floor( Date.now() / 1000 ).toString();
 		username = username ? timestamp + SEP + username : timestamp;
 
+		var csk = store.getCurrentSharedKey();
+		var hash = keys.genSharedKey( username, csk );
+
 		var result = {
 			username : username,
 			password : hash,
-			ttl      : 86400,
+			ttl      : ttl,
 			uris     : conf.urls
 		};
 
